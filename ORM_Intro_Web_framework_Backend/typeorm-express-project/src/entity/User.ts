cbcede68b356e380dac1f4486e48cddbd9db899e
Ayush Raj <ayusofayush@gmail.com>
2024-07-23 00:43:59 +0530

    session 17 corrected the minor project dev

--- ORM_Intro_Web_framework_Backend/typeorm-express-project/src/entity/User.ts
+++ ORM_Intro_Web_framework_Backend/typeorm-express-project/src/entity/User.ts
@@ -3,14 +3,14 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: number;
+  id!: number;
 
   @Column()
-  firstName: string;
+  firstName!: string;
 
   @Column()
-  lastName: string;
+  lastName!: string;
 
   @Column()
-  age: number;
+  age!: number;
 }
